bc91d861b1986db927691f6bb8110f3bacd06d50
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-02 15:19:21 +0200

    call the plot script more easy

---
+++ compare.sh
@@ -0,0 +1,5 @@
+python3 scatter_plot_timestamps.py \
+csv/orb_mono_c4-bag--ground_truth-odom.csv \
+csv/orb_mono_c4-bag--orb_slam3-camera_pose.csv \
+csv/orb-s1_stereo_orb_c4-bag--orb_slam3-camera_pose.csv \
+csv/orb-d1_d435_orb_c4-bag--orb_slam3-camera_pose.csv \
