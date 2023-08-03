e13e8bee2add84e1ca746a42277eb9b27129f4ba
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-03 15:35:49 +0200

    make better plot layout

--- histogram.py
+++ histogram.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
+
+plt.rc('axes', axisbelow=True)
+fontsize_xlabel = 12
+fontsize_ylabel = fontsize_xlabel
+labelsize_axes = 11
+suptitle = 'ORB stereo vs ground truth' # overall title on the very top
 
 # get command line arguments
 csv_file = sys.argv[1]
@@ -14,19 +21,22 @@ df['euclidean_distance'] = ((df['x_data'] - df['x_gt'])**2 + (df['y_data'] - df[
 df['euclidean_distance_delta'] = df['euclidean_distance'].diff()
 
 #plot histogram of euclidean distance and euclidean distance delta in subplot
-import matplotlib.pyplot as plt
 fig, ax = plt.subplots(2, 1)
-fig.tight_layout(h_pad=2)
+fig.suptitle(suptitle)
+ax[0].yaxis.grid(color='gray', linestyle='dashed')
 ax[0].hist(df['euclidean_distance'], bins=100)
-ax[0].set_title('Euclidean Distance')
-ax[0].set_xlabel('distance')
-ax[1].set_ylabel('count')
-
-
+ax[0].set_title('Euclidean distance')
+ax[0].set_xlabel('euclidean distance', fontsize=fontsize_xlabel)
+ax[0].set_ylabel('frequency', fontsize=fontsize_ylabel)
+ax[1].yaxis.grid(color='gray', linestyle='dashed')
 ax[1].hist(df['euclidean_distance_delta'], bins=100)
-ax[1].set_title('Euclidean Distance Delta')
-ax[1].set_xlabel('distance delta')
-ax[1].set_ylabel('count')
+ax[1].set_title('First discrete difference of euclidean distance')
+ax[1].set_xlabel('distance delta', fontsize=fontsize_xlabel)
+ax[1].set_ylabel('frequency', fontsize=fontsize_ylabel)
+ax[0].tick_params(axis='both', which='major', labelsize=labelsize_axes)
+ax[1].tick_params(axis='both', which='major', labelsize=labelsize_axes)
+
+fig.tight_layout(h_pad=2)
 # plt.show()
 
 #calculate the difference between the x and y columns of the ground truth and the data
@@ -34,28 +44,39 @@ df['x_diff'] = df['x_data'] - df['x_gt']
 df['y_diff'] = df['y_data'] - df['y_gt']
 #plot histogram of x_diff and y_diff in subplot
 fig, ax = plt.subplots(2, 1)
-fig.tight_layout(h_pad=2)
+fig.suptitle(suptitle)
+ax[0].yaxis.grid(color='gray', linestyle='dashed')
 ax[0].hist(df['x_diff'], bins=100)
-ax[0].set_title('X Difference')
-ax[0].set_xlabel('x difference')
-ax[0].set_ylabel('count')
+ax[0].set_title('Signed distance to ground truth in x')
+ax[0].set_xlabel('distance in x', fontsize=fontsize_xlabel)
+ax[0].set_ylabel('frequency', fontsize=fontsize_ylabel)
+ax[1].yaxis.grid(color='gray', linestyle='dashed')
 ax[1].hist(df['y_diff'], bins=100)
-ax[1].set_title('Y Difference')
-ax[1].set_xlabel('y difference')
-ax[1].set_ylabel('count')
+ax[1].set_title('Signed distance to ground truth in y')
+ax[1].set_xlabel('distance in y', fontsize=fontsize_xlabel)
+ax[1].set_ylabel('frequency', fontsize=fontsize_ylabel)
+ax[0].tick_params(axis='both', which='major', labelsize=labelsize_axes)
+ax[1].tick_params(axis='both', which='major', labelsize=labelsize_axes)
+fig.tight_layout(h_pad=2)
 # plt.show()
 
 # plot the diffs against the time stamp
 fig, ax = plt.subplots(2, 1)
-fig.tight_layout(h_pad=2)
+fig.suptitle(suptitle)
+ax[0].yaxis.grid(color='gray', linestyle='dashed')
 ax[0].plot(df['stamp'], df['x_diff'])
-ax[0].set_title('X Difference')
-ax[0].set_xlabel('time')
-ax[0].set_ylabel('x difference')
+ax[0].set_title('Signed distance to ground truth in x')
+ax[0].set_xlabel('time', fontsize=fontsize_xlabel)
+ax[0].set_ylabel('distance in x', fontsize=fontsize_ylabel)
+ax[1].yaxis.grid(color='gray', linestyle='dashed')
 ax[1].plot(df['stamp'], df['y_diff'])
-ax[1].set_title('Y Difference')
-ax[1].set_xlabel('time')
-ax[1].set_ylabel('y difference')
+ax[1].set_title('Signed distance to ground truth in y')
+ax[1].set_xlabel('time', fontsize=fontsize_xlabel)
+ax[1].set_ylabel('distance in y', fontsize=fontsize_ylabel)
+ax[0].tick_params(axis='both', which='major', labelsize=labelsize_axes)
+ax[1].tick_params(axis='both', which='major', labelsize=labelsize_axes)
+fig.tight_layout(h_pad=2)
+
 plt.show()
 
 
