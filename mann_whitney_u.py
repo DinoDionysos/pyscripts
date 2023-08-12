b4f5e2ec23c260d82ef9bca60699e85e816725be
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-12 02:49:28 +0200

    compare x trajectories and print mean pvalue

--- mann_whitney_u.py
+++ mann_whitney_u.py
@@ -2,129 +2,104 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import os
+
+#input
+# 1: folder path with rotated csv files
+# 2: folder path with rotated csv files to compare with
+# 3: range start to compare pairwise folder_1 and folder_2
+# 3: range end to compare pairwise folder_1 and folder_2
+
+folder_1 = sys.argv[1]
+folder_2 = sys.argv[2]
+range_1 = int(sys.argv[3])
+range_2 = int(sys.argv[4])
+
+df_list_1 = []
+df_list_2 = []
+
+# get all the names of the csv in the folder and read them in
+for filename in os.listdir(folder_1):
+    if filename.endswith('.csv'):
+        df_list_1.append(pd.read_csv(os.path.join(folder_1, filename)))
+for filename in os.listdir(folder_2):
+    if filename.endswith('.csv'):
+        df_list_2.append(pd.read_csv(os.path.join(folder_2, filename)))
+
+from scipy.stats import mannwhitneyu
+alpha_mwu = 0.05
+pvalues_mwu_test = []
+for i in range(int(range_1), int(range_2)):
+    distances_1 = df_list_1[i]['dist'].to_numpy()
+    distances_2 = df_list_2[i]['dist'].to_numpy()
+    #perform a mann whitney u test and print the results
+    stat_x, p = mannwhitneyu(distances_1, distances_2, alternative ='two-sided')
+    pvalues_mwu_test.append(p)
+    stat_y = distances_1.shape[0] * distances_2.shape[0] - stat_x
+    print('mwu stat 1 = %.1f, stat 2 = %.1f, p=%.15f' % (stat_x, stat_y, p), end='')
+    if p > alpha_mwu:
+        print('Same distribution (fail to reject H0) pvalue=%d' % p)
+    else:
+        print('Different distribution (reject H0) pvalue=%d' % p)
+pmean_mwu_test = np.mean(pvalues_mwu_test)
+print('mean pvalue = %.15f' % pmean_mwu_test)
+# print hello without newline after
+print('hello', end='')
+
+# alpha_ks = 0.05
+# pvalues_ks_test = []
+# for i in range(int(range_1), int(range_2)):
+#     distances_1 = df_list_1[i]['euclidean_distance'].to_numpy()
+#     distances_2 = df_list_2[i]['euclidean_distance'].to_numpy()
+#     # kologorov smirnov test
+#     print('--------------------------------------------------')
+#     from scipy.stats import ks_2samp
+#     stat, p = ks_2samp(distances_1, distances_2)
+#     pvalues_ks_test.append(p)
+#     print('KS Statistics=%.3f, p=%.15f' % (stat, p), end='')
+#     if p > alpha_ks:
+#         print('Same distribution (fail to reject H0)')
+#     else:
+#         print('Different distribution (reject H0)')
+# pmean_ks_test = np.mean(pvalues_ks_test)
+# print('mean pvalue = %.15f' % pmean_ks_test)
 
 
 
-# read the two csv files from the command line
-df_1 = pd.read_csv(sys.argv[1])
-df_2 = pd.read_csv(sys.argv[2])
+# def S(a,b):
+#     if a > b:
+#         return 1
+#     elif a == b:
+#         return 0.5
+#     elif a < b:
+#         return 0
 
-factor_meter = 1000
-df_1['x'] *= factor_meter 
-df_1['y'] *= factor_meter
-df_1['x_gt'] *= factor_meter
-df_1['y_gt'] *= factor_meter
-df_2['x'] *= factor_meter
-df_2['y'] *= factor_meter
-df_2['x_gt'] *= factor_meter
-df_2['y_gt'] *= factor_meter
+# def mwu_statistic(data_1, data_2):
+#     sum = 0 
+#     for i in range(len(data_1)):
+#         for j in range(len(data_2)):
+#             sum += S(data_1[i], data_2[j])
+#     return sum
 
-# calculate x_diff by x - x_gt
-df_1['x_diff'] = df_1['x'] - df_1['x_gt']
-df_2['x_diff'] = df_2['x'] - df_2['x_gt']
-# calculate y_diff by y - y_gt
-df_1['y_diff'] = df_1['y'] - df_1['y_gt']
-df_2['y_diff'] = df_2['y'] - df_2['y_gt']
-# calculate euclidean distance by sqrt(x_diff^2 + y_diff^2)
-df_1['euclidean_distance'] = ((df_1['x_diff'])**2 + (df_1['y_diff'])**2)**0.5
-df_2['euclidean_distance'] = ((df_2['x_diff'])**2 + (df_2['y_diff'])**2)**0.5
-# calculate the first discrete difference of the euclidean_distance column
-df_1['euclidean_distance_delta'] = df_1['euclidean_distance'].diff()
-df_2['euclidean_distance_delta'] = df_2['euclidean_distance'].diff()
+# print('self calculated mwu statistic')
+# print('Statistics 1 = %.3f \nStatistics 2 = %.3f' %(mwu_statistic(distances_1, distances_2),mwu_statistic(distances_2, distances_1)))
+# print('--------------------------------------------------')
 
-def S(a,b):
-    if a > b:
-        return 1
-    elif a == b:
-        return 0.5
-    elif a < b:
-        return 0
-
-def mwu_statistic(data_1, data_2):
-    sum = 0 
-    for i in range(len(data_1)):
-        for j in range(len(data_2)):
-            sum += S(data_1[i], data_2[j])
-    return sum
-
-distances_1 = df_1['euclidean_distance'].to_numpy()
-distances_2 = df_2['euclidean_distance'].to_numpy()
-# make in mm
-distances_1 = distances_1
-distances_2 = distances_2
-# drop the first row of both dataframes (0 or nan in both cases)
-distances_1 = distances_1[1:]
-distances_2 = distances_2[1:]
-# shorten both distances to 700
-# distances_1 = distances_1[:700]
-# distances_2 = distances_2[:700]
-# cut of everything bigger than the variance
-# distances_1 = distances_1[distances_1 < np.var(distances_1)]
-# distances_2 = distances_2[distances_2 < np.var(distances_2)]
-print('variance of distances_1 = %.15f' % np.var(distances_1))
-print('variance of distances_2 = %.15f' % np.var(distances_2))
-print('standard deviation of distances_1 = %.15f' % np.std(distances_1))
-print('standard deviation of distances_2 = %.15f' % np.std(distances_2))
-print('mean of distances_1 = %.15f' % np.mean(distances_1))
-print('mean of distances_2 = %.15f' % np.mean(distances_2))
-print('median of distances_1 = %.15f' % np.median(distances_1))
-print('median of distances_2 = %.15f' % np.median(distances_2))
-print('--------------------------------------------------')
+# print('--------------------------------------------------')
+# print('variance of distances_1 = %.15f' % np.var(distances_1))
+# print('variance of distances_2 = %.15f' % np.var(distances_2))
+# print('standard deviation of distances_1 = %.15f' % np.std(distances_1))
+# print('standard deviation of distances_2 = %.15f' % np.std(distances_2))
+# print('mean of distances_1 = %.15f' % np.mean(distances_1))
+# print('mean of distances_2 = %.15f' % np.mean(distances_2))
+# print('median of distances_1 = %.15f' % np.median(distances_1))
+# print('median of distances_2 = %.15f' % np.median(distances_2))
+# print('--------------------------------------------------')
 # remove outlier bigger than 0.025 and smaller than -0.025
 # distances_1 = distances_1[distances_1 < 0.025]
 # distances_1 = distances_1[distances_1 > -0.025]
 # distances_2 = distances_2[distances_2 < 0.025]
 # distances_2 = distances_2[distances_2 > -0.025]
 # cut distances from 650 to -120
-distances_1 = distances_1[650:-120]
-distances_2 = distances_2[650:-120]
-
-
-
-
-
-
-print('self calculated mwu statistic')
-print('Statistics 1 = %.3f \nStatistics 2 = %.3f' %(mwu_statistic(distances_1, distances_2),mwu_statistic(distances_2, distances_1)))
-print('--------------------------------------------------')
-#perform a mann whitney u test
-from scipy.stats import mannwhitneyu
-stat_x, p = mannwhitneyu(distances_1, distances_2, alternative ='two-sided')
-stat_y = distances_1.shape[0] * distances_2.shape[0] - stat_x
-print('mwu Statistics 1 = %.1f \nmwu Statistics 2 = %.1f \np=%.15f' % (stat_x, stat_y, p))
-
-# interpret
-alpha = 0.05
-if p > alpha:
-    print('Same distribution (fail to reject H0)')
-else:
-    print('Different distribution (reject H0)')
-
-print('--------------------------------------------------')
-temp = distances_1
-distances_1 = distances_2
-distances_2 = temp
-del temp
-stat_x, p = mannwhitneyu(distances_1, distances_2, alternative ='two-sided')
-stat_y = distances_1.shape[0] * distances_2.shape[0] - stat_x
-print('mwu Statistics 1 = %.1f \nmwu Statistics 2 = %.1f \np=%.15f' % (stat_x, stat_y, p))
-
-# interpret
-alpha = 0.05
-if p > alpha:
-    print('Same distribution (fail to reject H0)')
-else:
-    print('Different distribution (reject H0)')
-
-# kologorov smirnov test
-print('--------------------------------------------------')
-from scipy.stats import ks_2samp
-stat, p = ks_2samp(distances_1, distances_2)
-print('KS Statistics=%.3f \np=%.15f' % (stat, p))
-# interpret
-alpha = 0.05
-if p > alpha:
-    print('Same distribution (fail to reject H0)')
-else:
-    print('Different distribution (reject H0)')
-
+# distances_1 = distances_1[650:-120]
+# distances_2 = distances_2[650:-120]
