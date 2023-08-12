7cd257ec3b7543438293287e6cfa37ba98e21826
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-12 13:02:31 +0200

    code clean up

--- rotate_coor.py
+++ rotate_coor.py
@@ -13,76 +13,54 @@ plt.rcParams.update({
 pos_fig_x = 1200
 pos_fig_y = 100
 folder_name = sys.argv[1].split('/')[2]
-print(folder_name) 
 
-# load the csv second argument 
 df_gt = pd.read_csv(sys.argv[1])
-# load the csv for the argument
 df_data = pd.read_csv(sys.argv[2])
 
-# get the timestamp column of both dataframes
 df_data_timestamp = df_data['stamp']
 df_gt_timestamp = df_gt['stamp']
-# normalize the timestamp column
 df_data_timestamp = df_data_timestamp - df_data_timestamp[0]
 df_gt_timestamp = df_gt_timestamp - df_gt_timestamp[0]
-#convert to numpy array
 df_data_timestamp = df_data_timestamp.to_numpy()
 df_gt_timestamp = df_gt_timestamp.to_numpy()
 
-# save the x and y column of the ground truth to numpy array
+# prepare true_points and mapping_points arrays for kabsch algorithm
 gt_x = df_gt['x'].to_numpy()
 gt_y = df_gt['y'].to_numpy()
-# interpolate the data_x and data_y to the length of gt_x and gt_y
 gt_x = np.interp(df_data_timestamp, df_gt_timestamp, gt_x)
 gt_y = np.interp(df_data_timestamp, df_gt_timestamp, gt_y)
-# combine with a zero array as the z column
 gt_z = np.zeros(len(gt_x))
-# combine into one numpy array
 true_points = np.array([gt_x, gt_y, gt_z])
 
-# save the x and y column of the data to numpy array
 data_x = df_data['x'].to_numpy()
 # if sys.arg[2] contains orb and d435 and not imu then get the z axis instead of the y axis
 if 'orb' in sys.argv[2] and 'd435' in sys.argv[2] and 'imu' not in sys.argv[2]:
     data_y = df_data['z'].to_numpy()
 else:
     data_y = df_data['y'].to_numpy()
-
-# zero arry as z column
 data_z = np.zeros(len(data_x))
-# combine into one numpy array
 mapping_points = np.array([data_x, data_y, data_z])
 
-#transpose
 true_points = true_points.T
 mapping_points = mapping_points.T
-
-
 # kabsch algorithm from https://stackoverflow.com/questions/60877274/optimal-rotation-in-3d-with-kabsch-algorithm
 # as far as I understand it resembles the horn method implementations that I have seen like ein horn_demo.py. The difference seems laut wikipedia that horn is in qaternion and kabsch is in rotation matrix.
 #For an alternative kabsch implementation see here https://zpl.fi/aligning-point-patterns-with-kabsch-umeyama-algorithm/
-
 mapped_centroid = np.average(mapping_points, axis=0)
 true_centroid = np.average(true_points, axis=0)
-
 mapping_points -= mapped_centroid
 true_points -= true_centroid
-
 h = mapping_points.T @ true_points
 u, s, vt = np.linalg.svd(h)
 v = vt.T
-
 d = np.linalg.det(v @ u.T)
 # get the sign of d
-d = np.sign(d) 
 # solution from stackoverflow does not implement the sign here? wikipedia says it should be and the horn_demo.py also does it.
 # https://en.wikipedia.org/wiki/Kabsch_algorithm
+d = np.sign(d) 
 e = np.array([[1, 0, 0], [0, 1, 0], [0, 0, d]])
-
 r = v @ e @ u.T
 tt = true_centroid - np.matmul(r, mapped_centroid)
-
 true_points += true_centroid
 mapping_points += mapped_centroid
 
@@ -93,41 +71,21 @@ for i in mapping_points:
     map_list.append(np.reshape(point, (1, 3)))
 mapped_xyz = np.vstack(map_list)
 
-
 # normalize such that the first point is 0,0
-true_points -= true_points[0]
-mapping_points -= mapping_points[0]
-mapped_xyz -= mapped_xyz[0]
+# true_points -= true_points[0]
+# mapping_points -= mapping_points[0]
+# mapped_xyz -= mapped_xyz[0]
 
-# calculate the euclidean distance between the mapped_xyz and true_points
 euclidean_distance = np.linalg.norm(mapped_xyz - true_points, axis=1)
-# calculate the diff of the euclidean distance
 euclidean_distance_diff = np.diff(euclidean_distance)
-# append a zero to the euclidean_distance_diff at the front
 euclidean_distance_diff = np.insert(euclidean_distance_diff, 0, 0)
-# *1000
 euclidean_distance_diff = euclidean_distance_diff * 1000
 euclidean_distance = euclidean_distance * 1000
-# make a dataframe from the mapped_xyz, true_points and timestamps and euclidean_distance and euclidean_distance_diff
 df_data = pd.DataFrame({'stamp': df_data_timestamp, 'x': mapped_xyz[:,0], 'y': mapped_xyz[:,1], 'x_gt': true_points[:,0], 'y_gt': true_points[:,1], 'dist': euclidean_distance, 'ffd': euclidean_distance_diff})
 
-
-# split the argument 1 by '/' and get the last part
-csv_file = 'csv/aligned/' + folder_name +'/'+ sys.argv[1].split('/')[-1].split('.')[0]
-csv_file += '_vs_' + sys.argv[2].split('/')[1].split('--')[0] + '.csv'
-# save the dataframe as csv
-# if folder_name does not exist create it
-if not os.path.exists('csv/aligned/' + folder_name):
-    os.makedirs('csv/aligned/' + folder_name)
-print(csv_file)
-df_data.to_csv(csv_file, index=False)
-
-# keep reference to figure
 fig = plt.figure()
-# plot the x y of mapped_xyz and true_points
-# make the scale of x and y equal
 plt.axis('equal')
-# for all points with the same timestamp plot a line between them
+plt.title(sys.argv[1].split('/')[-1] + ' vs ' + sys.argv[2].split('/')[1])
 for i in range(len(true_points)):
     plt.plot([true_points[i,0], mapped_xyz[i,0]], [true_points[i,1], mapped_xyz[i,1]], color='gray')
 plt.plot(true_points[:,0], true_points[:,1], marker='o', markeredgecolor='black', label='true')
@@ -138,13 +96,19 @@ geom = mngr.window.geometry()
 x,y,dx,dy = geom.getRect()
 mngr.window.setGeometry(pos_fig_x, pos_fig_y, dx*2, dy*2)
 
-# print the accumulated euclidean distance
 print('accumulated euclidean distance: ', np.sum(euclidean_distance))
-# print the accumulated euclidean distance diff
-print('accumulated euclidean distance diff: ', np.sum(euclidean_distance_diff))# print the accumulated euclidean distance diff but absolute
+print('accumulated euclidean distance diff: ', np.sum(euclidean_distance_diff))
 print('accumulated euclidean distance diff abs: ', np.sum(np.abs(euclidean_distance_diff)))
-plt.show(block=True)
-plt.pause(2)
+
+csv_file = 'csv/aligned/' + folder_name +'/'+ sys.argv[1].split('/')[-1].split('.')[0]
+csv_file += '_vs_' + sys.argv[2].split('/')[1].split('--')[0] + '.csv'
+if not os.path.exists('csv/aligned/' + folder_name):
+    os.makedirs('csv/aligned/' + folder_name)
+df_data.to_csv(csv_file, index=False)
+print('saved csv to: ' + csv_file)
+
+plt.show(block=False)
+plt.pause(5)
 plt.close(fig)
 
 
