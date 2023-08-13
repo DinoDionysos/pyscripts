0731a433c3e97aeebc18708b6632e5d42c3bf28e
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-14 01:12:55 +0200

    minor change

--- do_bag2csv.sh
+++ do_bag2csv.sh
@@ -2,18 +2,17 @@
 #$1: start index for the files
 #$2: end index
 
-
-file_prefix="c7_orb_stereo"
+file_prefix="c8_orb_d435"
 folder="orb"
 folder="$folder/$file_prefix" # later add /$file_prefix_XX.* (see below)
 #results in data of form */$folder/$file_prefix/$file_prefix_XX.*
 
 
-for i in $(seq $1 1 $2)
-do
-    python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag gt
-    python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag orb
-done
+# for i in $(seq $1 1 $2)
+# do
+#     python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag gt
+#     python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag orb
+# done
 
 for i in $(seq $1 1 $2)
 do
