6a8694631a9f589515e2168551b20045d2b7c7da
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-16 22:21:03 +0200

    uncomment

--- do_bag2csv.sh
+++ do_bag2csv.sh
@@ -8,17 +8,17 @@ folder="$folder/$file_prefix" # later add /$file_prefix_XX.* (see below)
 #results in data of form */$folder/$file_prefix/$file_prefix_XX.*
 
 
-# for i in $(seq $1 1 $2)
-# do
-#     python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag gt
-#     python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag orb
-# done
+for i in $(seq $1 1 $2)
+do
+    python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag gt
+    python3 bag2csv.py bags/$folder/$file_prefix"_"$i.bag orb
+done
 
 for i in $(seq $1 1 $2)
 do
     python3 rotate_coor.py \
     csv/$folder/$file_prefix"_"$i-gt.csv \
     csv/$folder/$file_prefix"_"$i-orb.csv \
-    0 # how long to show the plot
+    1 # how long to show the plot
 done
 
