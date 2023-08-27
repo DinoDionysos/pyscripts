67ab6442f7d4b177f48f26714839e16ab121c545
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-28 01:59:23 +0200

    get the orientation

--- bag2csv.py
+++ bag2csv.py
@@ -63,30 +63,91 @@ print("[INFO] "+file_name+" | executing: " + command)
 
 df = pd.read_csv(csv_file)
 
-# define function to replace the following part
-if (topic_type == "nav_msgs/Odometry"):
-    df = df[df.columns.drop(list(df.filter(regex='twist')))]
-    df = df[df.columns.drop(list(df.filter(regex='covariance')))]
-    df = df[df.columns.drop(list(df.filter(regex='orientation')))]
-    df = df[df.columns.drop(list(df.filter(regex='frame_id')))]
-    df.columns = df.columns.str.replace('field.', '')
-    df.columns = df.columns.str.replace('pose.', '')
-    df.columns = df.columns.str.replace('position.', '')
-    df.columns = df.columns.str.replace('%', '')
-    df.columns = df.columns.str.replace('header.', '')
 
-elif (topic_type == "geometry_msgs/PoseStamped"):
-    # drop the all columns with twist in the name
-    df = df[df.columns.drop(list(df.filter(regex='twist')))]
-    df = df[df.columns.drop(list(df.filter(regex='covariance')))]
-    df = df[df.columns.drop(list(df.filter(regex='orientation')))]
-    df = df[df.columns.drop(list(df.filter(regex='frame_id')))]
-    # remove the "field." from the column names
-    df.columns = df.columns.str.replace('field.', '')
-    df.columns = df.columns.str.replace('pose.', '')
-    df.columns = df.columns.str.replace('position.', '')
-    df.columns = df.columns.str.replace('%', '')
-    df.columns = df.columns.str.replace('header.', '')
+# drop the all columns with twist in the name
+df = df[df.columns.drop(list(df.filter(regex='twist')))]
+df = df[df.columns.drop(list(df.filter(regex='covariance')))]
+df = df[df.columns.drop(list(df.filter(regex='frame_id')))]
+# remove the "field." from the column names
+df.columns = df.columns.str.replace('field.', '')
+df.columns = df.columns.str.replace('pose.', '')
+df.columns = df.columns.str.replace('position.', '')
+df.columns = df.columns.str.replace('orientation.', 'quat_')
+df.columns = df.columns.str.replace('%', '')
+df.columns = df.columns.str.replace('header.', '')
 
 df.to_csv(csv_file, index=False)
 # print("[INFO] "+file_name+" | saved to: " + csv_file)
+
+
+
+
+
+
+
+
+
+def drop_and_replace(df, message_type):
+    if (message_type == "nav_msgs/Odometry"):
+        df = df[df.columns.drop(list(df.filter(regex='twist')))]
+        df = df[df.columns.drop(list(df.filter(regex='covariance')))]
+        df = df[df.columns.drop(list(df.filter(regex='frame_id')))]
+        df.columns = df.columns.str.replace('field.', '')
+        df.columns = df.columns.str.replace('pose.', '')
+        df.columns = df.columns.str.replace('position.', '')
+        df.columns = df.columns.str.replace('position.', '')
+        df.columns = df.columns.str.replace('%', '')
+        df.columns = df.columns.str.replace('header.', '')
+
+    elif (message_type == "geometry_msgs/PoseStamped"):
+        # drop the all columns with twist in the name
+        df = df[df.columns.drop(list(df.filter(regex='twist')))]
+        df = df[df.columns.drop(list(df.filter(regex='covariance')))]
+        df = df[df.columns.drop(list(df.filter(regex='frame_id')))]
+        # remove the "field." from the column names
+        df.columns = df.columns.str.replace('field.', '')
+        df.columns = df.columns.str.replace('pose.', '')
+        df.columns = df.columns.str.replace('position.', '')
+        df.columns = df.columns.str.replace('position.', '')
+        df.columns = df.columns.str.replace('%', '')
+        df.columns = df.columns.str.replace('header.', '')
+
+
+
+# for the comparison of the qat2rpy functions
+# import copy
+# count=0
+# count_12 = 0
+# count_13 = 0
+# count_23 = 0
+# precision = 6
+# #check if all the yaw values are the same
+# for i in range(len(df['yaw'])):
+#     # yaw in 6 precision but dont change the value in the df
+#     yaw = copy.deepcopy(df['yaw'][i])
+#     yaw = round(yaw, precision)
+#     yaw2 = copy.deepcopy(df['yaw2'][i])
+#     yaw2 = round(yaw2, precision)
+#     yaw3 = copy.deepcopy(df['yaw3'][i])
+#     yaw3 = round(yaw3, precision)
+
+#     if yaw != yaw2 or yaw != yaw3 or yaw2 != yaw3:
+#         # print("[INFO] "+file_name+" | yaw values are not the same at index: " + str(i))
+#         # print("[INFO] "+file_name+" | yaw: " + str(yaw))    
+#         # print("[INFO] "+file_name+" | yaw2: " + str(yaw2))
+#         # print("[INFO] "+file_name+" | yaw3: " + str(yaw3))
+#         #print the differences
+#         if yaw != yaw2:
+#             # print("[INFO] "+file_name+" | yaw: " + str(yaw) + " | yaw2: " + str(yaw2)+ " | diff: " + str(yaw-yaw2) + " | index: " + str(i))
+#             count_12 +=1
+#         if yaw != yaw3:
+#             # print("[INFO] "+file_name+" | yaw: " + str(yaw) + " | yaw3: " + str(yaw3)+ " | diff: " + str(yaw-yaw3) + " | index: " + str(i))
+#             count_13 +=1
+#         if yaw2 != yaw3:
+#             # print("[INFO] "+file_name+" | yaw2: " + str(yaw2) + " | yaw3: " + str(yaw3)+ " | diff: " + str(yaw2-yaw3) + " | index: " + str(i))
+#             count_23 +=1
+#         count +=1
+# print("[INFO] "+file_name+" | count: " + str(count) + " of " + str(len(df['yaw'])))
+# print("[INFO] "+file_name+" | count_12: " + str(count_12))
+# print("[INFO] "+file_name+" | count_13: " + str(count_13))
+# print("[INFO] "+file_name+" | count_23: " + str(count_23))
