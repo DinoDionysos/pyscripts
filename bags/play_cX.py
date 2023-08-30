aa46533751adcdbd0a00c7a7004f6c370394302c
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-30 14:35:37 +0200

    play and record

--- bags/play_cX.py
+++ bags/play_cX.py
@@ -1,17 +1,36 @@
 import os
 import sys
+path='/media/dino/Samsung_T5/rosbags/'
+folder_rosbags = path
+short_cut = sys.argv[1]
+#get all the folders in the path
+folders = os.listdir(path)
+#check if one of the directory starts with the short_cut
+for folder in folders:
+    if folder.startswith(short_cut):
+        print(folder)
+        if os.path.isdir(path + folder):
+            print(folder)
+            #if so, set the folder to path + folder
+            folder_rosbags = path + folder
+            print('playing rosbags in folder: ' + folder_rosbags)
+            break
+if folder == path:
+    print('playing from default folder: ' + folder_rosbags)
 
-folder = sys.argv[1]
 # get all files in the folder
-files = os.listdir(folder)
+files = os.listdir(folder_rosbags)
 # put a string together with "rosbag play " + all files
-command = "rosbag play "
+command = "rosbag play --skip-empty=1 "
 for file in files:
-    command += folder + "/" + file + " "
+    # if file starts with short_cut and ends with .bag
+    if file.startswith(short_cut) and file.endswith(".bag"):
+        # add the file to the command
+        command += folder_rosbags + "/" + file + " "
 # print the command
 print(command)
-# pause for 5 seconds
-os.system("sleep 5")
+# pause for 1 seconds
+os.system("sleep 1")
 
 # execute the command
 os.system(command)
