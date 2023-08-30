83accf279bea2cb6f2e7326ddbb36f2e67c8f994
Daniel Bachus <ai21m014@technikum-wien.at>
2023-08-30 14:59:08 +0200

    merge with windows branch by hand

--- bags/n_record_slam.sh
+++ bags/n_record_slam.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Call it from pyscripts/bags folder because of the relative paths
-# $1: folder name where the folder $2 is inside (f.e. orb)
-# $2: prefix and folder name of the bagfile to record (c7_orb_d435)
-# $3: postfix of the roslaunch file
-# $4: number of runs / how many bagfiles to record 
+# $1: shortcut of the bag file (c11, c12, c13, ...)
+# $2: folder name where the folder $2 will be inside (f.e. orb)
+# $3: postfix of the roslaunch file (stereo, d435, mono)
+# $4: number of runs / how many bagfiles to record (1, 25, ...)
+# for example: bash n_record_slam.sh orb c11 d435 25
+
+# $2 old: prefix and folder name of the bagfile to record (c7_orb_d435)
+#make a variable for the folder name out of $2_$1_$3 (c7_orb_d435)
+folder_and_prefix=$1_$2_$3
 
 for i in $(seq 1 1 $4)
 do
@@ -15,13 +20,13 @@ do
   echo "----------------------------------------------------------------"
   echo "Starting recording"
   sleep 0.5
-  bash record_orb.sh $1 $2 &
+  bash record_orb.sh $2 $folder_and_prefix &
   sleep 1
   echo "----------------------------------------------------------------"
   echo "Starting playing"
   sleep 0.5
   # rosbag play c7_circle_lockstep_c4_frac.bag
-  python3 play_cX.py c8
+  python3 play_cX.py /mnt/d/rosbags/ $1
   sleep 2
   killall record
   sleep 1
@@ -32,9 +37,3 @@ do
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 done
 
-
-
-# $1: postfix of the roslaunch file
-# $2: prefix and folder name of the bagfile (c7_orb_d435)
-# $3: folder name where the folder $2 is inside (f.e. orb/)
-# $4: number of runs / how many bagfiles to record 
