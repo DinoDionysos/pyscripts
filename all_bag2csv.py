4b0cbe962d795c30f2c52a8c94bb70566f53edeb
Daniel Bachus <ai21m014@technikum-wien.at>
2023-09-09 01:36:00 +0200

    make slam_type array for the for loops

--- all_bag2csv.py
+++ all_bag2csv.py
@@ -7,15 +7,15 @@ bag2csv_flag = False
 # rotate_coor_flag = True
 rotate_coor_flag = True
 
-show_plot_for = 2
+show_plot_for = -1
 
-start_idx = 0
-end_idx = 19
+start_idx = 21
+end_idx = 21
 indices = range(start_idx, end_idx+1)
 # indices = [7,10,12,16,17]
 
 # bag_indices = [9, 15, 28, 19, 17, 51, 49, 34]
-bag_indices = [15, 28, 19, 17, 51, 49, 34]
+bag_indices = [8]#, 28, 19, 17, 51, 49, 34]
 # bag_indices = [9]
 # complete list of those who have results
 # bag_indices = [9, 14, 15, 16, 17, 19, 21, 28, 29, 33, 34, 46, 47, 48, 49, 50, 51]
@@ -23,10 +23,13 @@ bag_indices = [15, 28, 19, 17, 51, 49, 34]
 folder_orb = "orb"
 folder_ssd = "/mnt/d"
 
+slam_types = ['mono']
+
 ##### stuff that do_bag2csv.sh does translated in python
 if bag2csv_flag:
     for bag_idx in bag_indices:
-        for slam_type in ['stereo','mono', 'd435']:
+        # for slam_type in ['stereo','mono', 'd435']:
+        for slam_type in slam_types:
             folder_ssd = "/mnt/d/"
             file_prefix = "c" + str(bag_idx) + "_" + folder_orb + "_" + slam_type
             folder_orb = folder_orb
@@ -38,7 +41,7 @@ if rotate_coor_flag:
     for bag_idx in bag_indices:
         for i in indices:
             shortest_length_csv = -1
-            for slam_type in ['mono', 'stereo', 'd435']:
+            for slam_type in slam_types:
                 short_cut_data = folder_orb
                 file_prefix = "c" + str(bag_idx) + "_" + folder_orb + "_" + slam_type
                 number = str(i)
@@ -50,7 +53,7 @@ if rotate_coor_flag:
                     shortest_length_csv = length_csv
                 # print("shortest_length_csv: " + str(shortest_length_csv))
             # now we have the shortest length of all csv files for this scenario with N=number
-            for slam_type in ['stereo', 'mono', 'd435']:
+            for slam_type in slam_types:
                 file_prefix = "c" + str(bag_idx) + "_" + folder_orb + "_" + slam_type
                 rotate_coor(short_cut_data, str(i), show_plot_for, file_prefix, folder_ssd, folder_orb, shortest_length_csv)
             
